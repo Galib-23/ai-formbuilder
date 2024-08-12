2e7210e30702d779871a94a71c3ca50b6c720565
Galib-23 <aagalib2323@gmail.com>
2024-08-13 01:36:14 +0600

    add ai model and get response

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
+import CreateForm from "./_components/CreateForm";
 
 const Dashboard = () => {
   return (
-    <div>
-      ds
+    <div className="p-10">
+      <h2 className="font-bold text-3xl flex items-center justify-between">
+        Dashboard <CreateForm />
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- configs/schema.js
+++ configs/schema.js
@@ -1,9 +1,9 @@
-const { integer, text, varchar, timestamp } = require("drizzle-orm/pg-core");
+const { text, varchar, serial } = require("drizzle-orm/pg-core");
 const { pgTable } = require("drizzle-orm/pg-core");
 
-export const jsonForms = pgTable('jsonForms', {
-    id: integer('id').primaryKey(),
-    jsonform: text('jsonform').notNull(), 
-    createdBy: varchar('createdBy', 255).notNull(),
-    createdAt: timestamp('createdAt').notNull(),
+export const jsonForms = pgTable("jsonForms", {
+  id: serial("id").primaryKey(),
+  jsonform: text("jsonform").notNull(),
+  createdBy: varchar("createdBy", 255).notNull(),
+  createdAt: varchar("createdAt", 255).notNull(),
 });
