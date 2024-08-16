b3b30bdf3a985f7e726ddcec38d08d86e387c6dd
Galib-23 <aagalib2323@gmail.com>
2024-08-16 11:56:02 +0600

    take user responses from the user

--- app/aiform/[formid]/page.jsx
+++ app/aiform/[formid]/page.jsx
@@ -16,11 +16,41 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Checkbox } from "@/components/ui/checkbox";
 
 const LiveAiForm = ({ params }) => {
-  const [record, setRecord] = useState(null);
   const [jsonForm, setJsonForm] = useState(null);
   const [selectedTheme, setSelectedTheme] = useState("");
   const [selectedBg, setSelectedBg] = useState("");
   const [formStyle, setFormStyle] = useState("");
+  const [formData, setFormData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSelectChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+
+  const handleCheckBoxChange = (fieldName, itemName, value) => {
+    const list = formData?.[fieldName] ? formData?.[fieldName] : [];
+    if (value) {
+      list.push({
+        label: itemName,
+        value: value
+      });
+      setFormData({...formData, [fieldName]: list})
+    }else {
+      const result = list.filter((item) => item.label == itemName);
+      setFormData({...formData, [fieldName]: result})
+    }
+  }
+
 
   useEffect(() => {
     params && GetFormData();
@@ -31,8 +61,6 @@ const LiveAiForm = ({ params }) => {
       .select()
       .from(jsonForms)
       .where(eq(jsonForms.id, Number(params?.formid)));
-
-    setRecord(result[0]);
     setJsonForm(JSON.parse(result[0].jsonform));
     setSelectedTheme(result[0].theme);
     setSelectedBg(result[0].background);
@@ -40,11 +68,12 @@ const LiveAiForm = ({ params }) => {
   };
 
   return (
-    <div
+    <form
       className="p-4 flex min-h-screen items-center justify-center"
       style={{
         backgroundImage: selectedBg,
       }}
+      onSubmit={handleSubmit}
     >
       <div
         className={`border p-5 rounded-lg ${formStyle}`}
@@ -60,10 +89,13 @@ const LiveAiForm = ({ params }) => {
           <div className="flex items-center gap-2" key={idx}>
             {field?.fieldType == "select" ? (
               <div className="my-3 w-full">
-                <label className="text-sm text-gray-800">
+                <label className={`text-sm text-gray-800`}>
                   {field?.fieldLabel}
+                  <span className="text-red-500">
+                    {field?.required && " *"}
+                  </span>
                 </label>
-                <Select>
+                <Select required={field?.required} onValueChange={(value) => handleSelectChange(field?.fieldName, value)}>
                   <SelectTrigger className="w-full bg-transparent">
                     <SelectValue placeholder="Select" />
                   </SelectTrigger>
@@ -78,13 +110,16 @@ const LiveAiForm = ({ params }) => {
               </div>
             ) : field?.fieldType == "radio" ? (
               <div className="my-3 w-full">
-                <label className="text-sm text-gray-800">
+                <label className={`text-sm text-gray-800`}>
                   {field?.fieldLabel}
+                  <span className="text-red-500">
+                    {field?.required && " *"}
+                  </span>
                 </label>
-                <RadioGroup>
+                <RadioGroup required={field?.required}>
                   {field?.options?.map((item, idx) => (
                     <div key={idx} className="flex items-center space-x-2 my-1">
-                      <RadioGroupItem value={item.value} id={item.value} />
+                      <RadioGroupItem value={item.value} id={item.value} onClick={() => handleSelectChange(field.fieldName, item.label)} />
                       <Label htmlFor={item.label}>{item.label}</Label>
                     </div>
                   ))}
@@ -92,40 +127,50 @@ const LiveAiForm = ({ params }) => {
               </div>
             ) : field?.fieldType == "checkbox" ? (
               <div className="my-3 w-full">
-                <label className="text-sm text-gray-800">
+                <label className={`text-sm text-gray-800`}>
                   {field?.fieldLabel}
+                  <span className="text-red-500">
+                    {field?.required && " *"}
+                  </span>
                 </label>
                 {field?.options ? (
                   field?.options?.map((item, idx) => (
                     <div key={idx} className="flex gap-2 items-center">
-                      <Checkbox />
+                      <Checkbox onCheckedChange={(v) => handleCheckBoxChange(field?.fieldLabel, item?.label, v)} />
                       <h2>{item.label}</h2>
                     </div>
                   ))
                 ) : (
                   <div className="flex gap-2 items-center">
-                    <Checkbox />
+                    <Checkbox required={field?.required} />
                     <h2>{field?.fieldLabel || field?.label}</h2>
                   </div>
                 )}
               </div>
             ) : (
               <div className="my-3 w-full">
-                <label className="text-sm text-gray-800">
+                <label className={`text-sm text-gray-800`}>
                   {field?.fieldLabel}
+                  <span className="text-red-500">
+                    {field?.required && " *"}
+                  </span>
                 </label>
                 <Input
+                  required={field?.required}
                   type={field?.fieldType}
                   placeholder={field?.placeholder}
                   name={field?.fieldName}
+                  onChange={(e) => handleInputChange(e)}
                 />
               </div>
             )}
           </div>
         ))}
-        <button className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
